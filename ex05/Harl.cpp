e90991e3c0f2d1da48af04be0386ced83153dd79
joeow06 <joeow06@gmail.com>
2025-10-17 02:39:45 +0800

    fixed output

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 01:38:32 by jow               #+#    #+#             */
-/*   Updated: 2025/10/17 02:07:52 by jow              ###   ########.fr       */
+/*   Updated: 2025/10/17 02:38:26 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ void Harl::debug(void)
 
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put "
 "enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for"
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for "
 "years whereas you started working here since last month." << std::endl;
 }
 
